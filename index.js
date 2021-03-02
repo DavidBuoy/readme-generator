46665df9278e7e924459f8f99cf3f3873042cb82
David Bushard <davidbushard@gmail.com>
2021-03-01 17:32:55 -0800

    Got it to where it to work

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
+// External Package
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
+
+// Internal Package
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your FIRST and LAST name?',
+        },
+
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub user name?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Email Address?',
+        },
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 'subProjectName',
+            message: 'If needed, Please give your project a description name - ie: Homework or Test Project. ',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please give a short description of your project.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license below',
+            choices: ['MIT', 'APACHE', 'GPL', 'BSD', 'none',]
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What command should be run to run the test?',
+        },
+
+    ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions().then((answers) => {
+        try {
+            const md = generateMarkdown(answers);
+            
+            fs.writeFileSync('README.md', md);
+            
+            console.log('Successfully wrote to README.md');
+        }
+        catch (error) {
+            console.log(error);
+        }
+    });
+};
+
+// TODO: Create a function to write README file
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateReadme(answers) {
+  return `
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+<h1 align="center">${answers.projectName}</h1>
+<h5 align="center">${answers.name} </h5> 
+<h5 align="center">${answers.subProjectName} </h5>
+<br>
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contact Info ](#Contact-Info:)
+<br>
+<br>
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+## Description
+${answers.description}
+
+<br>
+<br>
+
+## Installation
+${answers.install}
+<br>
+<br>
+
+## Usage
+${answers.usage}
+<br>
+<br>
+
+## License
+
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen?style=for-the-badge)
+<br>
+This application is covered by the ${answers.license} license.
+<br>
+<br>
+<hr style="border:1px solid white"> </hr>
+<br>
+
+## Contact Info:
+${answers.name}
+
+Find me on GitHub:
+
+<a href="https://github.com/${answers.github}">
+        <img alt="GitHub Link" src="https://img.shields.io/badge/GitHub-${answers.github}-brightgreen?style=for-the-badge&logo=github" target="_blank" />
+</a>
+
+<br>
+Email me with any questions: ${answers.email}
+<br>
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateReadme;
